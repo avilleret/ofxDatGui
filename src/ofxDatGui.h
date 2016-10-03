e93df70b8c146ba6161397ca4c79e0f47a0b9cb6
Antoine Villeret <antoine.villeret@gmail.com>
2016-10-03 08:31:38 +0200

    make layoutGui() public

--- src/ofxDatGui.h
+++ src/ofxDatGui.h
@@ -100,6 +100,9 @@ class ofxDatGui : public ofxDatGuiInteractiveObject
         ofxDatGuiFolder* getFolder(string label);
         ofxDatGuiDropdown* getDropdown(string label);
     
+
+        void layoutGui();
+
     private:
     
         int mIndex;
@@ -134,7 +137,6 @@ class ofxDatGui : public ofxDatGuiInteractiveObject
         static unique_ptr<ofxDatGuiTheme> theme;
     
         void init();
-        void layoutGui();
         void anchorGui();
         void moveGui(ofPoint pt);
         bool hitTest(ofPoint pt);
